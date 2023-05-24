2cff8196c07e7aa6a3afdfd2ca6c45013266b2d8
mohammad_hasan <mryazdani98@outlook.com>
2023-05-25 00:17:25 +0330

    form added

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -8,8 +8,27 @@ namespace ConsoleApp1
 {
     class Program
     {
+        public static void form()
+        {
+            Console.WriteLine("hi and welcome");
+            Console.WriteLine("1.see the user lists");
+            Console.WriteLine("2.adding users");
+            Console.WriteLine("3.removing user");
+            Console.WriteLine("4.exit");
+        }
+        
         static void Main(string[] args)
         {
+            form();
+            Console.ReadLine();
+
+
+
         }
+
+        
     }
+    
 }
+
+
